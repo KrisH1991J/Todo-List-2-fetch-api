972a18772c7672dc56dc2019d1df4a18a1eb9832
KrisH1991J <kherman2859@gmail.com>
2021-03-03 19:38:00 +0000

    Todo-List-2-fetch-api

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,130 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
-export function Home() {
+const Home = () => {
+	const list = [];
+	const [todoList, updateTodos] = useState([]);
+	const apiURL =
+		"https://assets.breatheco.de/apis/fake/todos/user/KrisH1991J";
+
+	useEffect(() => {
+		fetch(apiURL)
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => {
+				updateTodos(data);
+				console.log(data);
+			})
+			.catch(error => {
+				console.log(error);
+				fetch(apiURL, {
+					method: "POST",
+					body: JSON.stringify([]),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(resp => {
+						return resp.json();
+					})
+					.then(data => {
+						console.log(data);
+					})
+					.catch(error => {
+						console.log(error);
+					});
+			});
+	}, []);
+
+	let addTask = e => {
+		let userInput = e.target.value;
+		if (userInput >= null) return;
+		if (e.keyCode == 13) {
+			let userTask = e.target.value;
+			let obj = { label: e.target.value, done: false };
+			let newList = [...todoList, obj];
+			updateTodos(newList);
+			e.target.value = "";
+			fetch(apiURL, {
+				method: "PUT",
+				body: JSON.stringify(newList),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(resp => {
+					return resp.json();
+				})
+				.then(data => {
+					console.log(data);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		}
+	};
+
+	let deleteTask = i => {
+		let updatedList = todoList.filter((element, index) => index !== i);
+		updateTodos(updatedList);
+		fetch(apiURL, {
+			method: "PUT",
+			body: JSON.stringify(updatedList),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => {
+				console.log(data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+		if (updatedList == 0) {
+			fetch(apiURL, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(resp => {
+					return resp.json();
+				})
+				.then(data => {
+					console.log(data);
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1>{`Today's Tasks:`}</h1>
+			<input
+				type="text"
+				name="task"
+				placeholder="What needs to be done?"
+				onKeyDown={addTask}
+			/>
+			<p>{todoList.length} items left</p>
+			<ul>
+				{todoList.map((todo, index) => (
+					<li key={index}>
+						{todo.label}{" "}
+						<button onClick={() => deleteTask(index)}>
+							<i className="fas fa-times"></i>
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
-}
+};
+
+export default Home;
